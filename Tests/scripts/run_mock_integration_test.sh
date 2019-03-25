c85bdd4b68202c0d01af2c836ae8af9d3921eaef
benparadise <benjoparadise@gmail.com>
2019-03-25 17:16:58 +0200

    Create execution shell script.

---
+++ Tests/scripts/run_mock_integration_test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+echo "start content tests"
+
+SECRET_CONF_PATH=$(cat secret_conf_path)
+SERVER_IP=$(cat public_ip)
+SERVER_URL="https://$SERVER_IP"
+CONF_PATH="./Tests/conf.json"
+USERNAME=$(cat $SECRET_CONF_PATH | jq '.username')
+PASSWORD=$(cat $SECRET_CONF_PATH | jq '.userPassword')
+
+# remove quotes from password
+temp="${PASSWORD%\"}"
+temp="${temp#\"}"
+PASSWORD=$temp
+
+# remove quotes from username
+temp="${USERNAME%\"}"
+temp="${temp#\"}"
+USERNAME=$temp
+
+[ -n "${NIGHTLY}" ] && IS_NIGHTLY=true || IS_NIGHTLY=false
+
+python ./Tests/scripts/mocks_integration_test.py -u "$USERNAME" -p "$PASSWORD" -s "$SERVER_URL" -c "$CONF_PATH" -e "$SECRET_CONF_PATH" -n $IS_NIGHTLY -t "$SLACK_TOKEN" -a "$CIRCLECI_TOKEN" -b "$CIRCLE_BUILD_NUM" -g "$CIRCLE_BRANCH" --isAMI true -d "Demisto GA"
